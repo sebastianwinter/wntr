a9ad9b192c013f5e7f62c120485719e275f1b1cf
Sebastian W <hallo@sebastianwinter-design.de>
2021-01-23 01:42:06 +0100

    prismic api (unfinished)

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,18 @@
 import '../scss/app.scss';
+//import Prismic from '@prismicio/client';
+import Prismic from 'prismic-javascript';
 
 // Your JS Code goes here
 document.querySelector('p').innerHTML = "app.js says hello :)"
+
+//var Prismic = require('prismic-javascript');
+ 
+Prismic.api("http://wntr.prismic.io/api", function(error, api) {
+  var options = {req : req}; // In Node.js, pass the request as 'req' to read the reference from the cookies
+  api.query("", options, function(err, response) { // An empty query will return all the documents
+    if (err) {
+      console.log("Something went wrong: ", err);
+    }
+    console.log("Documents: ", response.documents);
+  });
+});;
